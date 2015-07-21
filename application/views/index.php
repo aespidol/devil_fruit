26c866c3bbcd1f76e433b82fc68034b344dd8340
Andrew Espidol <aespidol@gmail.com>
2015-07-20 19:50:16 -0700

    Added CSS and created initial index.php view

--- application/views/index.php
+++ application/views/index.php
@@ -1,34 +1,69 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Login</title>
+	<title>Devil's Market</title>
+    <link type="text/css" rel="stylesheet" href="/assets/materialize/css/materialize.min.css"  media="screen,projection"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 </head>
-<body>
-	<h1>Register</h1>
-	<?php if(isset($errors))
-	{
-		echo $errors;
-	} 
-	unset($errors);
-	if(isset($success))
-	{
-		echo $success;	
-	}
-	unset($success);
-	?>
-	<form action="/register" method="post">
-		<p>Name: <input type="text" name="name"></p>
-		<p>Username: <input type="text" name="username"></p>
-		<p>Email: <input type="text" name="email"></p>
-		<p>Password: <input type="password" name="password"></p>
-		<p>Confirm: <input type="password" name="confirm_password"></p>
-		<p><input type="submit" name="submit" value="Register"></p>
-	</form>
-	<h1>Login</h1>
-	<form action="/login" method="post">
-		<p>Email: <input type="text" name="email"></p>
-		<p>Password: <input type="password" name="password"></p>
-		<p><input type="submit" name="submit" value="Login"></p>
-	</form>
+<body class="grey lighten-3">
+  	<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+  	<script type="text/javascript" src="/assets/materialize/js/materialize.min.js"></script>
+<!-- Nav Bar		 -->
+	<div class="navbar-fixed">
+	<nav>
+	  <div class="nav-wrapper grey darken-4">
+	    <a href="#!" class="brand-logo center"><img src="/assets/logo.png" alt=""></a>
+	    <ul class="left hide-on-med-and-down">
+	      <li><a href="/home">Home</a></li>
+	      <li><a href="/about">About</a></li>
+	      <li><a href="/paramecia">Paramecia</a></li>
+	      <li><a href="/logia">Logia</a></li>
+	      <li><a href="/zoan">Zoan</a></li>
+	    </ul>
+	   	<ul class="right hide-on-med-and-down">
+	      <li><a href="/cart">Cart</a></li>
+	    </ul>
+	  </div>
+	</nav>
+	</div>
+<!-- End Navbar	     -->
+  	<div class="container ">
+  	<h2 class="center">Welcome to the Devil's Market</h2>
+  	<p class="flow-text center">Enhanced GMOs~Purest of Substances~Guaranteed Side Effects<br><span class="red">Caution: The ability to swim will be lost upon consumption.</span></p>
+  	<h1 class="center">Paramecia</h1>
+	  	<div class="row">
+	  		<div class="col s4">
+		  		<div class="card grey lighten-3 z-depth-0">
+		  			<img src="/assets/image/operation.png" alt="" class="responsive-img">
+		  		</div>
+		  		<h4 class="center">Ope Ope No Mi</h4>
+		  		<p class="center">The Ope Ope no Mi is a Paramecia-type Devil Fruit that allows its user to create a spherical space or "room", in which the user has complete control over the placement and orientation of the objects inside, making the user a Free Modification Human (改造自在人間 Kaizō Jizai Ningen?).[4] "Ope" comes from the word "Operation", as the space the user creates is compared to an "operating room" and the user to a surgeon.<a href=""> More Details..</a></p>
+		  		<div class="center">
+		  			<a class="waves-effect waves-light btn"href="">Add To Cart</a>
+		  		</div>
+  			</div>
+  			<div class="col s4">
+		  		<div class="card grey lighten-3 z-depth-0">
+		  			<img src="/assets/image/rubber_new.png" alt="" class="responsive-img">
+		  		</div>
+		  		<h4 class="center">Gomu Gomu No Mi</h4>
+		  			<p class="center">The Gomu Gomu no Mi is a Paramecia-type Devil Fruit that enables the user's body to stretch like rubber, making the user a Rubber Human (ゴム人間 Gomu Ningen?). "Gomu" means rubber in Japanese.<a href=""> More Details..</a></p>
+
+	  			<div class="center">
+	  			<a class="waves-effect waves-light btn">Add To Cart</a>
+	  			</div>
+		  	</div>
+		  	<div class="col s4">
+		  		<div class="card grey lighten-3 z-depth-0">
+		  			<img src="/assets/image/string.png" alt="" class="responsive-img">
+		  		</div>
+		  		<h4 class="center">Doku Doku No Mi</h4>	
+	  			<p class="center">The Doku Doku no Mi is a Paramecia-type Devil Fruit that grants the user the ability to produce and control different types of poison, as well as grant immunity to all forms of poison, making the user a Poison Human (毒人間 Doku Ningen?). "Doku" (毒) means "poison". <a href=""> More Details..</a></p>
+	  			<div class="center">
+	  			<a class="waves-effect waves-light btn">Add To Cart</a>
+	  			</div>
+		  	</div>
+	  	</div>	
+  	</div>
 </body>
 </html>
